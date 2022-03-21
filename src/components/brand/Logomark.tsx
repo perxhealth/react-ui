b4f4be7e83f31a328035e5a2744f5fab46fa205c
Luke Rollans <luke@rollans.sh>
2022-03-21 10:52:35 +0800

    A bit of progress but still broken when imported to another project

--- src/components/brand/Logomark.tsx
+++ src/components/brand/Logomark.tsx
@@ -1,7 +1,8 @@
+import * as React from "react"
 import { chakra, forwardRef, HTMLChakraProps } from "@chakra-ui/system"
 
 export interface LogomarkProps extends HTMLChakraProps<"img"> {}
 
-export const Logomark = forwardRef<LogomarkProps, 'img'>(props, ref) => {
+export const Logomark = forwardRef<LogomarkProps, "img">((props, ref) => {
   return <chakra.img ref={ref} {...props} />
-}
+})

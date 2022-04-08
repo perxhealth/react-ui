a70889b3f945780e2fe00e3de0f8049a9a931f8e
Luke Rollans <luke@rollans.sh>
2022-04-08 22:22:53 +0800

    Upgrade to webpack5

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
+import { extendedTheme } from "../src/theme"
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
-  actions: { argTypesRegex: '^on.*' },
-};
+  actions: { argTypesRegex: "^on.*" },
+  chakra: {
+    theme: extendedTheme,
+  },
+}

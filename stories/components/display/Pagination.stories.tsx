bbf89234b02666f3b0f38f527563fb30bb68782a
Luke Rollans <luke@rollans.sh>
2022-04-08 22:47:18 +0800

    Add Pagination story

---
+++ stories/components/display/Pagination.stories.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Meta, Story } from "@storybook/react"
+
+import {
+  Pagination,
+  PaginationProps,
+} from "../../../src/components/display/Pagination"
+
+const meta: Meta = {
+  title: "Components/Display/Pagination",
+  component: Pagination,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+const Template: Story<Props> = (args) => <Pagination {...args} />
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Default = Template.bind({})
+
+Default.args = {}

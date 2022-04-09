7f5e7b69add5783de50869671f72d50c30d83e36
Luke Rollans <luke@rollans.sh>
2022-04-09 16:56:19 +0800

    Increase pagination test coverage

--- src/components/display/__tests__/Pagination.test.tsx
+++ src/components/display/__tests__/Pagination.test.tsx
@@ -1,13 +1,48 @@
 import * as React from "react"
-import { screen, render } from "@testing-library/react"
+import { screen, render, cleanup } from "@testing-library/react"
 import user from "@testing-library/user-event"
 
 import { Pagination } from "../Pagination"
+
+afterEach(cleanup)
 
 describe("Pagination", () => {
   const onPreviousClick = jest.fn()
   const onNextClick = jest.fn()
   const onPageNumberClick = jest.fn()
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe("page count", () => {
+    beforeEach(() => {
+      render(
+        <Pagination
+          currentPage={5}
+          totalPages={42}
+          onPreviousClick={onPreviousClick}
+          onNextClick={onNextClick}
+          onPageNumberClick={onPageNumberClick}
+        />
+      )
+    })
+
+    it("renders the first button", () => {
+      user.click(screen.getByText("1"))
+      expect(onPageNumberClick).toHaveBeenCalledWith(1)
+    })
+
+    it("renders the last button", () => {
+      user.click(screen.getByText("42"))
+      expect(onPageNumberClick).toHaveBeenCalledWith(42)
+    })
+
+    it("renders the expected amount of buttons", () => {
+      // 42 page buttons plus previous and next buttons
+      expect(screen.queryAllByRole("button").length).toBe(44)
+    })
+  })
 
   describe("when on the first page", () => {
     beforeEach(() => {

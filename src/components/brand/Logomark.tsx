d7dd742f45c0a3c54daca335f8ee9aa91d78a3ec
Luke Rollans <luke@rollans.sh>
2022-04-09 13:09:42 +0800

    Add size prop to Logomark component

--- src/components/brand/Logomark.tsx
+++ src/components/brand/Logomark.tsx
@@ -1,11 +1,19 @@
 import * as React from "react"
 import { Image, ImageProps } from "@chakra-ui/react"
 
-export const Logomark = (props: ImageProps) => {
+export interface Props extends ImageProps {
+  size: number
+}
+
+export const Logomark = (props: Props) => {
+  const { size } = props
+
   return (
     <Image
       src="https://play-lh.googleusercontent.com/zW9pdKa1xnRcm4DI3v5nJC9UmNeHBEfUvBZmLaRu5BlwuLgN5tqo90vlkkhWY6bDcFc"
       alt="Perx Health"
+      width={size}
+      height={size}
       {...props}
     />
   )

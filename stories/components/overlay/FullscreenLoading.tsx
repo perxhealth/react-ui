605feb34463cfbb8261675feffae8530bc7c80cd
Jessica Harriman <jess@perxhealth.com>
2022-09-09 14:36:50 +1000

    add FullscreenLoading story

---
+++ stories/components/overlay/FullscreenLoading.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { Meta, Story } from "@storybook/react"
+
+import {
+  FullscreenLoading,
+  Props,
+} from "../../../src/components/overlay/FullscreenLoading"
+
+const meta: Meta = {
+  title: "Components/Overlay/FullscreenLoading",
+  component: FullscreenLoading,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+const Template: Story<Props> = (args) => {
+  return <FullscreenLoading {...args} />
+}
+
+export const Default = Template.bind({})
+export const NoMessage = Template.bind({})
+
+Default.args = {
+    message: "Retrieving information"
+}

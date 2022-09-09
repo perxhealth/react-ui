e4a8ecab5a5a289f6b3e62555a4470a0478eda58
Jessica Harriman <jess@perxhealth.com>
2022-09-09 14:24:56 +1000

    remove describe block

--- src/components/overlay/__tests__/FullscreenLoading.test.tsx
+++ src/components/overlay/__tests__/FullscreenLoading.test.tsx
@@ -4,15 +4,13 @@ import { screen, render } from "@testing-library/react"
 import { FullscreenLoading } from "../FullscreenLoading"
 
 describe("FullscreenLoading", () => {
-  describe("when all information is provided", () => {
-    beforeEach(() => {
-      render(<FullscreenLoading message="Retrieving account information" />)
-    })
+  beforeEach(() => {
+    render(<FullscreenLoading message="Retrieving account information" />)
+  })
 
-    it("displays correct header text", () => {
-      expect(
-        screen.getByText("Retrieving account information")
-      ).toBeInTheDocument()
-    })
+  it("shows the expected message text", () => {
+    expect(
+      screen.getByText("Retrieving account information")
+    ).toBeInTheDocument()
   })
 })

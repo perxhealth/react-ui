27f30b1e9ac71658aad7793824d9dc24f9e47b18
Luke Rollans <luke@rollans.sh>
2022-10-17 15:57:05 +0800

    fix: Add ConfirmationModal updates

--- src/components/overlay/__tests__/ConfirmationModal.test.tsx
+++ src/components/overlay/__tests__/ConfirmationModal.test.tsx
@@ -1,64 +1,122 @@
 import * as React from "react"
-import { screen, render } from "@testing-library/react"
+import { screen, render, waitFor } from "@testing-library/react"
 import user from "@testing-library/user-event"
 
-import { Text } from "@chakra-ui/react"
-import { ConfirmationModal } from "../ConfirmationModal"
+import { ConfirmationModal, ConfirmationModalProps } from "../ConfirmationModal"
 
 describe("ConfirmationModal", () => {
   const onConfirm = jest.fn()
   const onCancel = jest.fn()
 
-  describe("when all information is provided", () => {
-    beforeEach(() => {
-      render(
-        <ConfirmationModal
-          onConfirm={onConfirm}
-          onCancel={onCancel}
-          isOpen={true}
-          headerText="Please Confirm"
-        >
-          <Text>Are you sure you want to confirm?</Text>
-        </ConfirmationModal>
-      )
-    })
-
-    it("displays correct header text", () => {
-      expect(screen.getByText("Please Confirm")).toBeInTheDocument()
-    })
-
-    it("displays correct body text", () => {
-      expect(
-        screen.getByText("Are you sure you want to confirm?")
-      ).toBeInTheDocument()
-    })
-
-    it("clicking the confirm button triggers onConfirm", () => {
-      user.click(screen.getByText("Confirm"))
-      expect(onConfirm).toHaveBeenCalledTimes(1)
-    })
-
-    it("clicking the cancel button triggers onCancel", () => {
-      user.click(screen.getByText("Cancel"))
-      expect(onCancel).toHaveBeenCalledTimes(1)
-    })
+  beforeEach(() => {
+    jest.clearAllMocks()
   })
 
-  describe("when no header text is provided", () => {
-    beforeEach(() => {
-      render(
-        <ConfirmationModal
-          onConfirm={onConfirm}
-          onCancel={onCancel}
-          isOpen={true}
-        >
-          <Text>Please make your selection.</Text>
-        </ConfirmationModal>
-      )
+  // Helper to render a ConfirmationModal with sane default props and optional
+  // overrides
+  const renderComponent = (props?: Partial<ConfirmationModalProps>) => {
+    // Set the sane defaults
+    const defaultProps: ConfirmationModalProps = {
+      onConfirm,
+      onCancel,
+      isOpen: true,
+      children: <p>ConfirmationModal body content</p>,
+    }
+
+    // Merge them with `props` and render the modal
+    return render(<ConfirmationModal {...defaultProps} {...props} />)
+  }
+
+  describe("props", () => {
+    describe("onConfirm", () => {
+      it("is called when the Confirm button is clicked", () => {
+        renderComponent()
+        user.click(screen.getByText("Confirm"))
+        expect(onConfirm).toHaveBeenCalledTimes(1)
+      })
     })
 
-    it("renders alternative text", () => {
-      expect(screen.getByText("Are you sure?")).toBeInTheDocument()
+    describe("onCancel", () => {
+      it("is called when the Cancel button is clicked", () => {
+        renderComponent()
+        user.click(screen.getByText("Cancel"))
+        expect(onCancel).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    describe("initialFocusRef", () => {
+      it("sets focuses on the provided ref", async () => {
+        const ref = React.createRef<HTMLButtonElement>()
+
+        renderComponent({
+          initialFocusRef: ref,
+          children: (
+            <form>
+              <button>First Button</button>
+              <button ref={ref}>Second Button</button>
+            </form>
+          ),
+        })
+
+        await waitFor(() => {
+          expect(screen.getByText("Second Button")).toHaveFocus()
+        })
+      })
+    })
+
+    describe("isOpen", () => {
+      describe("is true", () => {
+        beforeEach(() => renderComponent({ isOpen: true }))
+
+        it("displays content", async () => {
+          await waitFor(() => {
+            expect(screen.getByText("Are you sure?")).toBeVisible()
+          })
+        })
+      })
+    })
+
+    describe("headerText", () => {
+      describe("default", () => {
+        it("is set to a sane value", () => {
+          renderComponent()
+          expect(screen.getByText("Are you sure?")).toBeInTheDocument()
+        })
+      })
+
+      describe("customised", () => {
+        beforeEach(() => renderComponent({ headerText: "Think again!" }))
+
+        it("is displayed in the modal", () => {
+          expect(screen.getByText("Think again!")).toBeInTheDocument()
+        })
+
+        it("overrides the default", () => {
+          expect(screen.queryByText("Are you sure?")).not.toBeInTheDocument()
+        })
+      })
+    })
+
+    describe("isLoading", () => {
+      describe("is true", () => {
+        beforeEach(() => renderComponent({ isLoading: true }))
+
+        it("disables the Cancel button", () => {
+          expect(screen.getByText("Cancel")).toBeDisabled()
+        })
+
+        it("manifests the 'Confirm' button as loading", () => {
+          expect(screen.getByText("Confirm")).not.toBeVisible()
+          expect(screen.getByText("Confirm").closest("button")).toBeDisabled()
+        })
+      })
+    })
+
+    describe("children", () => {
+      it("renders children in the modal's body", () => {
+        renderComponent({ children: <p>Super modal!</p> })
+        expect(screen.getByText("Super modal!")).toBeInTheDocument()
+      })
     })
   })
 })

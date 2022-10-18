80309e23c3646a9c9bcb48cbbc4958244ae81423
Luke Rollans <luke@rollans.sh>
2022-10-18 15:18:04 +0800

    feat: Add FormDrawer component

---
+++ src/components/overlay/FormDrawer.tsx
@@ -0,0 +1,72 @@
+import * as React from "react"
+
+import {
+  Button,
+  Drawer,
+  DrawerProps,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  Stack,
+} from "@chakra-ui/react"
+
+export interface FormDrawerProps extends DrawerProps {
+  formId: string
+  title: string
+  isOpen: boolean
+  isSubmitting: boolean
+  onClose: () => void
+  children: JSX.Element | JSX.Element[]
+}
+
+export const FormDrawer = (props: FormDrawerProps) => {
+  // Destructure props to use directly
+  const { formId, children, title, isOpen, isSubmitting, onClose, ...rest } = props
+
+  // Render an opinionated drawer specifically designed for displaying a form
+  //  1. Form controls are anchored to the bottom of the drawer
+  //  2. The drawer's body will scroll in the case of very long forms
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose} placement="right" {...rest}>
+      <DrawerOverlay />
+      <DrawerContent margin="4" rounded="lg">
+        <DrawerCloseButton mt="1.5" />
+        <DrawerHeader>{title}</DrawerHeader>
+
+        <DrawerBody>{children}</DrawerBody>
+
+        <DrawerFooter
+          bg="gray.50"
+          borderTop="solid 1px"
+          borderColor="gray.100"
+          roundedBottomLeft="lg"
+          roundedBottomRight="lg"
+          justifyContent="center">
+          <Stack direction="row" w="full">
+            <Button
+              variant="outline"
+              size="lg"
+              bg="white"
+              w="full"
+              onClick={onClose}
+              isDisabled={isSubmitting}>
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              size="lg"
+              w="full"
+              form={formId}
+              isLoading={isSubmitting}
+              isDisabled={isSubmitting}>
+              Submit
+            </Button>
+          </Stack>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
---
+++ src/components/overlay/__tests__/FormDrawer.test.tsx
@@ -0,0 +1,72 @@
+import * as React from "react"
+import { screen, render, RenderResult } from "@testing-library/react"
+import user from "@testing-library/user-event"
+
+import { FormDrawer } from "../FormDrawer"
+
+describe("FormDrawer", () => {
+  let view: RenderResult
+
+  const onClose = jest.fn()
+
+  beforeEach(() => {
+    view = render(
+      <FormDrawer
+        title="Schedule a dose"
+        formId="the-form-id"
+        onClose={onClose}
+        isOpen={true}
+        isSubmitting={false}
+        children={<p>Hello!</p>}
+      />
+    )
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it("renders the header", () => {
+    expect(screen.getByRole("banner").textContent).toBe("Schedule a dose")
+  })
+
+  it("renders the body", () => {
+    expect(screen.getByText("Hello!")).toBeInTheDocument()
+  })
+
+  it("attaches the submit button to the form", () => {
+    const submitButton = screen.getByRole("button", { name: "Submit" })
+    expect(submitButton).toHaveAttribute("type", "submit")
+    expect(submitButton).toHaveAttribute("form", "the-form-id")
+  })
+
+  it("closes when the Cancel button is clicked", () => {
+    user.click(screen.getByText("Cancel"))
+    expect(onClose).toHaveBeenCalledTimes(1)
+  })
+
+  describe("submitting state", () => {
+    beforeEach(() => {
+      view.rerender(
+        <FormDrawer
+          title="Schedule a dose"
+          formId="the-form-id"
+          onClose={onClose}
+          isOpen={true}
+          isSubmitting={true}
+          children={<p>Hello!</p>}
+        />
+      )
+    })
+
+    it("disables the cancel button", () => {
+      expect(screen.getByText("Cancel")).toBeDisabled()
+    })
+
+    it("disables the submit button", () => {
+      // 'Submit' text is actually in an off screen label given `isLoading`
+      // is also true. Because of this, we need to seek the underlying `button`
+      expect(screen.getByText("Submit").closest("button")).toBeDisabled()
+    })
+  })
+})

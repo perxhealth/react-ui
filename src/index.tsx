432ed840196c9a02486a3634286e529548c92f66
Luke Rollans <luke@rollans.sh>
2022-10-25 09:17:18 +0800

    perf: Consolidate exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { extendedTheme } from "./theme"
 
-export * from "./components"
+export * from "./components/display"
+export * from "./components/overlay"
 export * from "./components/feedback"
 export * from "./components/layout"
 export * from "./components/domain"

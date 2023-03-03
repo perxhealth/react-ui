08f0a73c1e786cea25b8e50fccf2d696932570cb
Luke Rollans <luke@rollans.sh>
2023-03-03 18:03:02 +0800

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 /// <reference types="vitest" />
+import { resolve } from "path"
 import { defineConfig } from "vite"
+
 import react from "@vitejs/plugin-react"
 import svgrPlugin from "vite-plugin-svgr"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
+  plugins: [react(), dts(), svgrPlugin()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "react-ui",
+      name: "ReactUI",
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",

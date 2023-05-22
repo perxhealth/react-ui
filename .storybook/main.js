deacf8f9d39076ae9209c8241e1ea5149112b8dc
Luke Rollans <luke@rollans.sh>
2023-05-22 14:16:46 +0800

    prettier formatting what was missed

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,21 @@
 module.exports = {
   stories: ["../stories/**/*.stories.@(ts|tsx|js|jsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@chakra-ui/storybook-addon"],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chakra-ui/storybook-addon",
+  ],
   typescript: {
-    check: true // type-check stories during Storybook build
+    check: true,
   },
-
   features: {
-    emotionAlias: false
+    emotionAlias: false,
   },
   framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
-};
+    autodocs: true,
+  },
+}

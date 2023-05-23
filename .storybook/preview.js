60ac654d0bb467ff980cfb9b0a0c0a299db7ac4d
Luke Rollans <luke@rollans.sh>
2023-05-23 15:34:56 +0800

    load up Averta font in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,14 @@
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
 import { extendedTheme } from "../src/theme"
+
+import "../src/styles/fonts.css"
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: "^on.*" },
   viewport: {
-    viewports: INITIAL_VIEWPORTS
+    viewports: INITIAL_VIEWPORTS,
   },
   chakra: {
     theme: extendedTheme,

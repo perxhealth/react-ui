cf5182e9aa226479597c0c09882c59dab892ab0c
Luke Rollans <luke@rollans.sh>
2023-08-29 23:10:09 +0800

    tweaking storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-actions",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
